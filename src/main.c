7b68c6244d263d72b06f50c96b5c0aa4f0066ec6
Oli1331 <zenaustimov234@gmail.com>
2026-05-06 21:46:43 +0700

    structuring

--- src/main.c
+++ src/main.c
@@ -62,10 +62,45 @@ typedef struct Vector_object_s {
 
 typedef enum Camera_mode_e {
     WIREFRAME_MODE,
-    BACK_FACES_MODE,
+    SOLID_WITHOUT_LIGHT_MODE,
     SOLID_MODE,
+
+
     CAMERA_MODE_COUNT
 }Camera_mode;
+
+typedef struct SDL_data_s {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Event event;
+
+    SDL_Texture* texture;
+
+    bool is_running;
+}SDL_data;
+
+typedef struct Scene_data_s {
+    Vector_mdl models;
+    Vector_obj objects;
+    Vector_vrtc global_render_bufer;
+    float* z_bufer;
+    Uint32* pixels;
+
+    float view_matrix[16];
+    float projection_matrix[16];
+    float mvp_matrix[16];
+
+    float FOV;
+    float z_near, z_far;
+
+    Vec light;
+
+    Vec camera;
+    float camera_speed;
+    Camera_mode mode;
+
+    bool reverse_normal;
+}Scene_data;
 
 void init_vector_vrtc(Vector_vrtc* v) {
     v->capacity = 1;v->cnt_vertices = 0;
@@ -276,15 +311,15 @@ void build_mvp_matrix(float model[16], float view[16], float proj[16], float mvp
     multiply_matrix_4x4(proj, temp_view_model, mvp);   //  Proj * (View * Model)
 }
 
-void build_view_matrix(float view_matrix[16], float displacement_x, float displacement_y, float displacement_z) {
+void build_view_matrix(float view_matrix[16], Vec camera) {
     for (int i = 0; i < 16; i++) view_matrix[i] = 0.0f;
     view_matrix[0] = 1.0f;
     view_matrix[5] = 1.0f;
     view_matrix[10] = 1.0f;
     view_matrix[15] = 1.0f;
-    view_matrix[0 * 4 + 3] = displacement_x;
-    view_matrix[1 * 4 + 3] = displacement_y;
-    view_matrix[2 * 4 + 3] = displacement_z;
+    view_matrix[0 * 4 + 3] = camera.x;
+    view_matrix[1 * 4 + 3] = camera.y;
+    view_matrix[2 * 4 + 3] = camera.z;
 }
 
 void build_projection_matrix(float FOV, float ratio_screen, float z_near, float z_far, float proj_matrix[16]) {
@@ -302,7 +337,7 @@ float edge_function(float x1, float y1, float x2, float y2, float x3, float y3) 
     return (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
 }
 
-void draw_triangle(float* z_bufer, float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3) {
+void draw_triangle(float inverse, float* z_bufer, float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3) {
     int min_x = fmaxf(0, fminf(x1, fminf(x2, x3)));
     int min_y = fmaxf(0, fminf(y1, fminf(y2, y3)));
     int max_x = fminf(SCREEN_WIDTH - 1, fmaxf(x1, fmaxf(x2, x3)));
@@ -318,7 +353,7 @@ void draw_triangle(float* z_bufer, float x1, float y1, float z1, float x2, float
             float w2 = edge_function(x3, y3, x1, y1, x, y);
             float w3 = edge_function(x1, y1, x2, y2, x, y);
 
-            if (w1 < 0 && w2 < 0 && w3 < 0) {
+            if ((inverse==0 && w1 < 0 && w2 < 0 && w3 < 0) || ((inverse  && w1 > 0 && w2 > 0 && w3 > 0))) {
 
                 w1 /= area; w2 /= area; w3 /= area;
 
@@ -334,12 +369,11 @@ void draw_triangle(float* z_bufer, float x1, float y1, float z1, float x2, float
     }
 }
 
-
-int main(int argc, char* argv[]) {
+void start_SDL(SDL_data* data) {
     // Инициализация видео-подсистемы
     SDL_Init(SDL_INIT_VIDEO);
     // Создание окна
-    SDL_Window* window = SDL_CreateWindow(
+    data->window = SDL_CreateWindow(
         "Core3D",          // Заголовок
         SDL_WINDOWPOS_CENTERED,      // X (по центру)
         SDL_WINDOWPOS_CENTERED,      // Y (по центру)
@@ -347,163 +381,242 @@ int main(int argc, char* argv[]) {
         SDL_WINDOW_SHOWN             // Флаги
     );
     // Создание рендерера (ускоренного видеокартой)
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    bool is_running = true;
-    SDL_Event event;
-    Vector_mdl models;  init_vector_mdl(&models);
-    Vector_obj objects; init_vector_obj(&objects);
-    Vector_vrtc global_render_bufer; init_vector_vrtc(&global_render_bufer);
-
-    SDL_Texture* texture = texture = SDL_CreateTexture(
-        renderer,                   // Твой SDL_Renderer*
+    data->renderer = SDL_CreateRenderer(data->window, -1, SDL_RENDERER_ACCELERATED);
+    data->texture = SDL_CreateTexture(
+        data->renderer,                   // Твой SDL_Renderer*
         SDL_PIXELFORMAT_RGBA8888,   // Формат пикселей (должен совпадать с твоим массивом)
         SDL_TEXTUREACCESS_STREAMING,// Важно! STREAMING оптимизирован для частого обновления
         SCREEN_WIDTH,                        // Ширина (width)
         SCREEN_HEIGHT                            // Высота (height)
     );
-    float* z_bufer = malloc(SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(float));
-    Uint32* pixels = malloc(SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(Uint32));
-    float view_matrix[16] = { 0 }; for (int i = 0; i < 4; i++) view_matrix[i * 4 + i] = 1;
-    float projection_matrix[16] = { 0 };
-    float mvp_matrix[16] = { 0 };
-    float FOV = 70;
-    float z_near = 0.01, z_far = 10000;
-    float angle = 0;
-    Camera_mode mode = WIREFRAME_MODE;
+    data->is_running = true;
+}
 
-    Vec camera = { 0 };
-    //Vec light = { 0 };
-    float camera_speed = 0.01;
+void start_scene(Scene_data* data) {
+    init_vector_mdl(&(data->models));
+    init_vector_obj(&(data->objects));
+    init_vector_vrtc(&(data->global_render_bufer));
+
+    data->z_bufer = malloc(SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(float));
+    data->pixels = malloc(SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(Uint32));
+    for (int i = 0; i < 4; i++) { // fill view_matrix
+        for (int j = 0; j < 4; j++) {
+            if (i == j)data->view_matrix[i * 4 + j] = 1;
+            else data->view_matrix[i * 4 + j] = 0;
+        }
+    }
+    for (int i = 0; i < 16; i++) {
+        data->projection_matrix[i] = 0;
+    }
+
+    data->FOV = 70;
+    data->z_near = 0.01, data->z_far = 10000;
+    data->mode = WIREFRAME_MODE;
+
+    data->camera.x = 0;
+    data->camera.y = 0;
+    data->camera.z = 0;
+    data->camera_speed = 0.01;
+
+    data->light.x = 0;
+    data->light.y = 0;
+    data->light.z = 0;
+
+    data->reverse_normal = 0;
+}
+
+// Обработка событий
+void event_handling(SDL_data* sdl, Scene_data* scene) {
+    while (SDL_PollEvent(&(sdl->event))) {
+        switch (sdl->event.type) {
+        case SDL_QUIT:
+            sdl->is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            switch (sdl->event.key.keysym.scancode) {
+            case SDL_SCANCODE_Z:
+                scene->mode = (scene->mode + 1) % CAMERA_MODE_COUNT;
+                fprintf(stderr, "CHANGE MODE to - %d\n", scene->mode);
+                break;
+
+            case SDL_SCANCODE_I:
+                scene->reverse_normal = (scene->reverse_normal + 1) % 2;
+                fprintf(stderr, "REVERSE NORMAL\n");
+                break;
+            default: break;
+            }
+            break;
+        }
+    }
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_UP])     scene->camera.z -= scene->camera_speed;
+    if (state[SDL_SCANCODE_DOWN])   scene->camera.z += scene->camera_speed;
+    if (state[SDL_SCANCODE_LEFT])   scene->camera.x += scene->camera_speed;
+    if (state[SDL_SCANCODE_RIGHT])  scene->camera.x -= scene->camera_speed;
+    if (state[SDL_SCANCODE_W])      scene->camera.y -= scene->camera_speed;
+    if (state[SDL_SCANCODE_S])      scene->camera.y += scene->camera_speed;
+
+}
+
+void wireframe(SDL_data* sdl, Scene_data* scene) {
+
+    SDL_SetRenderDrawColor(sdl->renderer, 0, 0, 0, 255);// цвета фона
+    SDL_RenderClear(sdl->renderer);
+    SDL_SetRenderDrawColor(sdl->renderer, 255, 255, 255, 255); // цвет линий
+    for (int o = 0; o < scene->objects.cnt_object; o++) {
+        Model* mdl = scene->objects.data[o].model;
+        build_mvp_matrix(scene->objects.data[o].transforms, scene->view_matrix, scene->projection_matrix, scene->mvp_matrix);
+
+        provide_vector_vrtc(&(scene->global_render_bufer), mdl->vertices.cnt_vertices);
+        for (int v = 0; v < mdl->vertices.cnt_vertices; v++) {
+            apply_transform(scene->mvp_matrix, (float*)&(mdl->vertices.data[v]), (float*)&(scene->global_render_bufer.data[v]));
+        }
+
+        for (int p = 0; p < mdl->polygons.cnt_polygon; p++) {
+
+            int* v = mdl->polygons.data[p].v;
+            float w1 = scene->global_render_bufer.data[v[0]].w;
+            float w2 = scene->global_render_bufer.data[v[1]].w;
+            float w3 = scene->global_render_bufer.data[v[2]].w;
+
+            if (w1 < scene->z_near || w2 < scene->z_near || w3 < scene->z_near || w1 > scene->z_far || w2 > scene->z_far || w3 > scene->z_far)continue;//clipping
+
+            float x1 = (scene->global_render_bufer.data[v[0]].x / w1 + 1) * 0.5 * SCREEN_WIDTH;
+            float x2 = (scene->global_render_bufer.data[v[1]].x / w2 + 1) * 0.5 * SCREEN_WIDTH;
+            float x3 = (scene->global_render_bufer.data[v[2]].x / w3 + 1) * 0.5 * SCREEN_WIDTH;
+
+            float y1 = (1 - scene->global_render_bufer.data[v[0]].y / w1) * 0.5 * SCREEN_HEIGHT;
+            float y2 = (1 - scene->global_render_bufer.data[v[1]].y / w2) * 0.5 * SCREEN_HEIGHT;
+            float y3 = (1 - scene->global_render_bufer.data[v[2]].y / w3) * 0.5 * SCREEN_HEIGHT;
+
+            float normal = edge_function(x1, y1, x2, y2, x3, y3); //векторное произведение ребер грани
+            if (scene->reverse_normal)normal *= -1;
+            if (normal > 0)continue;
+
+            SDL_RenderDrawLine(sdl->renderer, (int)x1, (int)y1, (int)x2, (int)y2);
+            SDL_RenderDrawLine(sdl->renderer, (int)x2, (int)y2, (int)x3, (int)y3);
+            SDL_RenderDrawLine(sdl->renderer, (int)x3, (int)y3, (int)x1, (int)y1);
+
+        }
+
+    }
+    SDL_RenderPresent(sdl->renderer);
+    // задержка
+    SDL_Delay(12);
+}
+
+void solid_without_light(SDL_data* sdl, Scene_data* scene) {
+    for (int o = 0; o < scene->objects.cnt_object; o++) {
+        Model* mdl = scene->objects.data[o].model;
+        build_mvp_matrix(scene->objects.data[o].transforms, scene->view_matrix, scene->projection_matrix, scene->mvp_matrix);
+
+        provide_vector_vrtc(&(scene->global_render_bufer), mdl->vertices.cnt_vertices);
+        for (int v = 0; v < mdl->vertices.cnt_vertices; v++) {
+            apply_transform(scene->mvp_matrix, (float*)&(mdl->vertices.data[v]), (float*)&(scene->global_render_bufer.data[v]));
+        }
+
+        for (int p = 0; p < mdl->polygons.cnt_polygon; p++) {
+
+            int* v = mdl->polygons.data[p].v;
+            float w1 = scene->global_render_bufer.data[v[0]].w;
+            float w2 = scene->global_render_bufer.data[v[1]].w;
+            float w3 = scene->global_render_bufer.data[v[2]].w;
+
+            if (w1 < scene->z_near || w2 < scene->z_near || w3 < scene->z_near || w1 > scene->z_far || w2 > scene->z_far || w3 > scene->z_far)continue;//clipping
+
+            float x1 = (scene->global_render_bufer.data[v[0]].x / w1 + 1) * 0.5 * SCREEN_WIDTH;
+            float x2 = (scene->global_render_bufer.data[v[1]].x / w2 + 1) * 0.5 * SCREEN_WIDTH;
+            float x3 = (scene->global_render_bufer.data[v[2]].x / w3 + 1) * 0.5 * SCREEN_WIDTH;
+
+            float y1 = (1 - scene->global_render_bufer.data[v[0]].y / w1) * 0.5 * SCREEN_HEIGHT;
+            float y2 = (1 - scene->global_render_bufer.data[v[1]].y / w2) * 0.5 * SCREEN_HEIGHT;
+            float y3 = (1 - scene->global_render_bufer.data[v[2]].y / w3) * 0.5 * SCREEN_HEIGHT;
+
+            float normal = edge_function(x1, y1, x2, y2, x3, y3); //векторное произведение ребер грани
+            if (scene->reverse_normal)normal *= -1;
+            if (normal > 0)continue;
+
+            float z1 = scene->global_render_bufer.data[v[0]].z / w1;
+            float z2 = scene->global_render_bufer.data[v[1]].z / w2;
+            float z3 = scene->global_render_bufer.data[v[2]].z / w3;
+            draw_triangle(scene->reverse_normal, scene->z_bufer, x1, y1, z1, x2, y2, z2, x3, y3, z3);
+
+        }
+
+    }
+    for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
+        if (scene->z_bufer[i] < BASE_VALUE_Z_BUFER)
+            scene->pixels[i] = RGBA(100, 100, 250, 255);
+        else
+            scene->pixels[i] = RGBA(0, 0, 100, 255);
+    }
+    SDL_UpdateTexture(sdl->texture, NULL, scene->pixels, SCREEN_WIDTH * sizeof(Uint32));
+    SDL_RenderCopy(sdl->renderer, sdl->texture, NULL, NULL);
+    SDL_RenderPresent(sdl->renderer);
+    // задержка
+    SDL_Delay(6);
+}
+
+int main(int argc, char* argv[]) {
+    SDL_data data_sdl;
+    start_SDL(&data_sdl);
+    Scene_data data_scene;
+    start_scene(&data_scene);
+    float angle = 0; // crutch
+
 
     if (argc > 1) {
-        if (parsing_obj_file(argv[1], &models, &objects) == 0) {
+        if (parsing_obj_file(argv[1], &(data_scene.models), &(data_scene.objects)) == 0) {
             fprintf(stderr, "FILE not found\n");
         }
     }
+
+
     fprintf(stderr, "START MAIN LOOP\n");
-    while (is_running) {
-        // Обработка событий
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                is_running = false;
-                break;
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.scancode) {
-                case SDL_SCANCODE_Z:
-                    mode = (mode + 1) % CAMERA_MODE_COUNT;
-                    fprintf(stderr, "CHANGE MODE to - %d\n", mode);
+    while (data_sdl.is_running) {
+        event_handling(&data_sdl, &data_scene);
 
-                    break;
-                default: break;
-                }
-                break;
-            }
-            if (event.type == SDL_QUIT) {
-                is_running = false;
-            }
-        }
-        const Uint8* state = SDL_GetKeyboardState(NULL);
-        if (state[SDL_SCANCODE_UP])     camera.z -= camera_speed;
-        if (state[SDL_SCANCODE_DOWN])   camera.z += camera_speed;
-        if (state[SDL_SCANCODE_LEFT])   camera.x += camera_speed;
-        if (state[SDL_SCANCODE_RIGHT])  camera.x -= camera_speed;
-        if (state[SDL_SCANCODE_W])      camera.y -= camera_speed;
-        if (state[SDL_SCANCODE_S])      camera.y += camera_speed;
 
-        // Логика
+        build_projection_matrix(data_scene.FOV, (float)SCREEN_WIDTH / SCREEN_HEIGHT, data_scene.z_near, data_scene.z_far, data_scene.projection_matrix);
+        build_view_matrix(data_scene.view_matrix, data_scene.camera);
+        for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) data_scene.z_bufer[i] = BASE_VALUE_Z_BUFER;
 
-        // Отрисовка
-        // Установка цвета фона
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        // отрисовка модели
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Белый
-
-        build_projection_matrix(FOV, (float)SCREEN_WIDTH / SCREEN_HEIGHT, z_near, z_far, projection_matrix);
-        build_view_matrix(view_matrix, camera.x, camera.y, camera.z);
-        for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
-            z_bufer[i] = BASE_VALUE_Z_BUFER;
-        }
-        objects.data[0].transforms[0 * 4 + 0] = cosf(angle);
-        objects.data[0].transforms[0 * 4 + 1] = -sinf(angle);
-        objects.data[0].transforms[1 * 4 + 0] = sinf(angle);
-        objects.data[0].transforms[1 * 4 + 1] = cosf(angle);
+        data_scene.objects.data[0].transforms[0 * 4 + 0] = cosf(angle);
+        data_scene.objects.data[0].transforms[0 * 4 + 1] = -sinf(angle);
+        data_scene.objects.data[0].transforms[1 * 4 + 0] = sinf(angle);
+        data_scene.objects.data[0].transforms[1 * 4 + 1] = cosf(angle);
         angle += 0.01;
-        for (int o = 0; o < objects.cnt_object; o++) {
-            Model* mdl = objects.data[o].model;
-            build_mvp_matrix(objects.data[o].transforms, view_matrix, projection_matrix, mvp_matrix);
 
-            provide_vector_vrtc(&global_render_bufer, mdl->vertices.cnt_vertices);
-            for (int v = 0; v < mdl->vertices.cnt_vertices; v++) {
-                apply_transform(mvp_matrix, (float*)&(mdl->vertices.data[v]), (float*)&(global_render_bufer.data[v]));
-            }
-
-            for (int p = 0; p < mdl->polygons.cnt_polygon; p++) {
-
-                int* v = mdl->polygons.data[p].v;
-                float w1 = global_render_bufer.data[v[0]].w;
-                float w2 = global_render_bufer.data[v[1]].w;
-                float w3 = global_render_bufer.data[v[2]].w;
-
-                if (w1 < z_near || w2 < z_near || w3 < z_near || w1 > z_far || w2 > z_far || w3 > z_far)continue;//clipping
-
-                float x1 = (global_render_bufer.data[v[0]].x / w1 + 1) * 0.5 * SCREEN_WIDTH;
-                float x2 = (global_render_bufer.data[v[1]].x / w2 + 1) * 0.5 * SCREEN_WIDTH;
-                float x3 = (global_render_bufer.data[v[2]].x / w3 + 1) * 0.5 * SCREEN_WIDTH;
-
-                float y1 = (1 - global_render_bufer.data[v[0]].y / w1) * 0.5 * SCREEN_HEIGHT;
-                float y2 = (1 - global_render_bufer.data[v[1]].y / w2) * 0.5 * SCREEN_HEIGHT;
-                float y3 = (1 - global_render_bufer.data[v[2]].y / w3) * 0.5 * SCREEN_HEIGHT;
-
-                float normal = ((x2 - x1) * (y3 - y1)) - ((y2 - y1) * (x3 - x1)); //векторное произведение ребер грани
-                if (mode == BACK_FACES_MODE)normal *= -1;
-                if (normal > 0)continue;
-
-                if (mode == SOLID_MODE) {
-                    float z1 = global_render_bufer.data[v[0]].z / w1;
-                    float z2 = global_render_bufer.data[v[1]].z / w2;
-                    float z3 = global_render_bufer.data[v[2]].z / w3;
-                    draw_triangle(z_bufer, x1, y1, z1, x2, y2, z2, x3, y3, z3);
-                }
-                if (mode == WIREFRAME_MODE || mode == BACK_FACES_MODE) {
-                    SDL_RenderDrawLine(renderer, (int)x1, (int)y1, (int)x2, (int)y2);
-                    SDL_RenderDrawLine(renderer, (int)x2, (int)y2, (int)x3, (int)y3);
-                    SDL_RenderDrawLine(renderer, (int)x3, (int)y3, (int)x1, (int)y1);
-                }
-            }
-
+        switch (data_scene.mode) {
+        case WIREFRAME_MODE:
+            wireframe(&data_sdl, &data_scene);
+            break;
+        case SOLID_WITHOUT_LIGHT_MODE:
+            solid_without_light(&data_sdl, &data_scene);
+            break;
+        case SOLID_MODE:
+            //solid(&data_sdl, &data_scene);
+            break;
+        default:
+            break;
         }
-        if (mode == SOLID_MODE) {
-            for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
-                if (z_bufer[i] < BASE_VALUE_Z_BUFER)
-                    pixels[i] = RGBA(100, 100, 250, 255);
-                else pixels[i] = RGBA(0, 0, 100, 255);
-            }
-            SDL_UpdateTexture(texture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
-            SDL_RenderCopy(renderer, texture, NULL, NULL);
-        }
-        // Вывод на экран
-        SDL_RenderPresent(renderer);
 
-        // задержка
-        SDL_Delay(10);
+
     }
 
     // Сохранение файла
     fprintf(stderr, "SAVE FILE\n");
-    save_output_file(argc, argv, &objects, &global_render_bufer);
+    save_output_file(argc, argv, &data_scene.objects, &data_scene.global_render_bufer);
 
     // Очистка ресурсов
     fprintf(stderr, "FREE MEMORY\n");
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    SDL_DestroyTexture(data_sdl.texture);
+    SDL_DestroyRenderer(data_sdl.renderer);
+    SDL_DestroyWindow(data_sdl.window);
     SDL_Quit();
-    free_vector_vrtc(&global_render_bufer);
-    free_vector_mdl(&models);
-    free_vector_obj(&objects);
+    free_vector_vrtc(&data_scene.global_render_bufer);
+    free_vector_mdl(&data_scene.models);
+    free_vector_obj(&data_scene.objects);
 
     return 0;
 }
